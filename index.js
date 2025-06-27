92fc05f8417a1205a613223db42ec85df9e40c29
Angellucy460 <angelliona38@gmail.com>
2025-06-27 22:32:16 +0300

    Using the Array Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// Returns all drivers that match the passed in name (case-insensitive)
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Returns drivers if the beginning of their name matches the provided string
+function fuzzyMatch(drivers, partial) {
+  return drivers.filter(driver => driver.startsWith(partial));
+}
+
+// Returns drivers whose `name` property matches the passed in string
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
